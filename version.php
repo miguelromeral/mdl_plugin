0bccf9ebe2136696a89dbbda59bba56f5e32479e
MiguelRomeral <miguel.romeral@gmail.com>
2018-06-19 10:47:44 +0200

    Commit inicial con varios ficheros.

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php
+
+
+/**
+ * Version information
+ *
+ * @package    mod
+ * @subpackage league
+ * @copyright  2018 onwards Miguel Romeral 
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version   = 2018061901;       // The current module version (Date: YYYYMMDDXX)
+$module->requires  = 2011112900;       // Requires this Moodle version
+$module->component = 'mod_league';     // Full name of the plugin (used for diagnostics)
+$module->cron      = 60;
